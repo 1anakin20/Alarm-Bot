767d782c0466e4f61f147f071de92a0bf2b898c2
Tomas Daniel Nieto <tomasdaniel09@hotmail.com>
2020-09-05 20:03:04 -0400

    Changed SayHello command

--- src/java/com/ComputerSquad/commands/fun/hello/SayHello.java
+++ src/java/com/ComputerSquad/commands/fun/hello/SayHello.java
@@ -1,4 +1,4 @@
-package com.ComputerSquad.commands;
+package com.ComputerSquad.commands.fun.hello;
 
 import com.jagrosh.jdautilities.command.Command;
 import com.jagrosh.jdautilities.command.CommandEvent;
@@ -6,7 +6,7 @@ import com.jagrosh.jdautilities.command.CommandEvent;
 public class SayHello extends Command {
 
 	public SayHello() {
-		this.name = "SayHello";
+		this.name = "hello";
 		this.help = "It says hello to you :)";
 	}
 
