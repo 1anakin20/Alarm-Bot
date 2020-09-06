243a1623df8a85d2303a465b40d8efa7c08f889c
Tomas Daniel Nieto <tomasdaniel09@hotmail.com>
2020-09-05 20:00:02 -0400

    Refactored Main.java

--- src/java/com/ComputerSquad/Main.java
+++ src/java/com/ComputerSquad/Main.java
@@ -1,43 +1,63 @@
 package com.ComputerSquad;
 
-import com.ComputerSquad.ClassAlarm.ClassAlarm;
-import com.ComputerSquad.commands.SayHello;
-import com.ComputerSquad.helpers.Token;
-import com.ComputerSquad.commands.CommandListener;
+import com.ComputerSquad.commands.ClassAlarm.ClassAlarm;
+import com.ComputerSquad.commands.ClassAlarm.Clock;
+import com.ComputerSquad.commands.fun.hello.SayHello;
 import com.jagrosh.jdautilities.command.CommandClientBuilder;
 import com.jagrosh.jdautilities.commons.waiter.EventWaiter;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
-import net.dv8tion.jda.api.entities.MessageActivity;
 
 import javax.security.auth.login.LoginException;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
 
 public class Main {
-
+	public static final String PREFIX = "++";
 	public static void main( String[] args ) {
-		// Create bot instance
-		String token = Token.readToken();
-//		CommandListener commandListener = new CommandListener();
+		/*
+		  The configuration file should be inside a folder called "user"
+		  With a new line for each piece of information needed
+		  Order:
+		  Bot token
+		  ID of the owner of the bot
+		 */
+		List<String> config = null;
+		try {
+			config = Files.readAllLines(Paths.get("user/configuration"));
+		} catch (IOException e) {
+			System.out.println("No file named 'configuration' inside the user folder found");
+		}
+
+		if (config.size() != 2) {
+			System.out.println("The configuration file is incorrect");
+			System.exit(1);
+		}
 
 		EventWaiter eventWaiter = new EventWaiter();
 
-		CommandClientBuilder client = new CommandClientBuilder();
+		CommandClientBuilder client = new CommandClientBuilder()
+				.setOwnerId(config.get(1))
+				.useDefaultGame()
+				.setPrefix(PREFIX);
 
-		client.useDefaultGame();
-		client.setPrefix("++");
-
-		client.addCommand(
-				new SayHello()
+		client.addCommands(
+				new SayHello(),
+				new ClassAlarm()
 		);
 
-		client.setOwnerId("owner");
-
+		JDA jda = null;
 		try {
-			JDA jda = JDABuilder.createDefault(token).build();
+			jda = JDABuilder.createDefault(config.get(0)).build();
 			jda.addEventListener(eventWaiter, client.build());
+			jda.awaitReady();
 		} catch (LoginException e) {
 			System.out.println("Error, couldn't login. Please verify the token");
 			System.exit(1);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
 		}
 
 		// Get the alarm clock running
