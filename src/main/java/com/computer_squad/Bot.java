bbbaa763dc1d023aec4c484a18a6910e1663b11a
Tomas Daniel Nieto <tomasdaniel09@hotmail.com>
2020-09-25 16:32:10 -0400

    Renamed Main to Bot

--- src/main/java/com/computer_squad/Bot.java
+++ src/main/java/com/computer_squad/Bot.java
@@ -17,7 +17,7 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.List;
 
-public class Main {
+public class Bot {
 	public static final String PREFIX = "++";
 
 	public static void main( String[] args ) {
