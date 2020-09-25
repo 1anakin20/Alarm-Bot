b5c0ba53c18e663e849161366300eaa8a4c069b2
Tomas Daniel Nieto <tomasdaniel09@hotmail.com>
2020-09-25 16:29:38 -0400

    Added javadocs to SayHello

--- src/main/java/com/computer_squad/command/fun/hello/SayHello.java
+++ src/main/java/com/computer_squad/command/fun/hello/SayHello.java
@@ -3,6 +3,9 @@ package com.computer_squad.command.fun.hello;
 import com.jagrosh.jdautilities.command.Command;
 import com.jagrosh.jdautilities.command.CommandEvent;
 
+/**
+ * Command to test if the bot is working
+ */
 public class SayHello extends Command {
 
 	public SayHello() {
